87ee5384300e4c0e68c84ba65e486070766af91d
Muhammad Hasan Raza <92hasanraza689@gmail.com>
2025-10-03 12:34:13 +0500

    updates

--- client/src/instance/api.js
+++ client/src/instance/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'https://staff-management-system-eh1t.vercel.app/',
+  baseURL: 'https://staff-management-system-eh1t.vercel.app/api/v1',
   withCredentials: true
 });
